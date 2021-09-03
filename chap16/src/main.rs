0c15b0de643defc816b9e3be6b806ee31eb03786
ieiriyuki <ieiriyuki@sur.bbiq.jp>
2021-09-03 22:07:01 +0900

    Update 18.1.3

--- chap16/src/main.rs
+++ chap16/src/main.rs
@@ -1,5 +1,8 @@
-use std::io;
+use std::error::Error;
+use std::io::{self, BufReader};
 use std::io::prelude::*;
+use std::fs::File;
+use std::path::PathBuf;
 
 fn grep<R>(target: &str, reader: R) -> io::Result<()>
     where R: BufRead
@@ -13,6 +16,30 @@ fn grep<R>(target: &str, reader: R) -> io::Result<()>
     Ok(())
 }
 
+fn grep_main() -> Result<(), Box<Error>> {
+    let mut args = std::env::args().skip(1);
+    let target = match args.next() {
+        Some(s) => s,
+        None => Err("usage: grep PATTERN FILE..")?
+    };
+    let files: Vec<PathBuf> = args.map(PathBuf::from).collect();
+
+    if files.is_empty() {
+        let stdin = io::stdin();
+        grep(&target, stdin.lock())?;
+    } else {
+        for file in files {
+            let f = File::open(file)?;
+            grep(&target, BufReader::new(f))?
+        }
+    }
+    Ok(())
+}
+
 fn main() {
+    let result = grep_main();
+    if let Err(err) = result {
+        let _ = writeln!(io::stderr(), "{}", err);
+    }
     println!("Hello, world!");
 }
