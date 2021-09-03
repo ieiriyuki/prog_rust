b4c9bf7c272ec8679795d0cc53e57537c7b2b0d4
ieiriyuki <ieiriyuki@sur.bbiq.jp>
2021-09-03 22:23:44 +0900

    Update 18.3a

--- chap16/src/main.rs
+++ chap16/src/main.rs
@@ -1,45 +1,24 @@
-use std::error::Error;
-use std::io::{self, BufReader};
-use std::io::prelude::*;
-use std::fs::File;
-use std::path::PathBuf;
+use std::net::TcpListener;
+use std::io;
+use std::thread::spawn;
 
-fn grep<R>(target: &str, reader: R) -> io::Result<()>
-    where R: BufRead
-{
-    for line_result in reader.lines() {
-        let line = line_result?;
-        if line.contains(target) {
-            println!("{}", line);
-        }
+fn echo_main(addr: &str) -> io::Result<()> {
+    let listener = TcpListener::bind(addr)?;
+    println!("listening on {}", addr);
+    loop {
+        let (mut stream, addr) = listener.accept()?;
+        println!("connection received from {}", addr);
+
+        let mut write_stream = stream.try_clone()?;
+        spawn(move || {
+            io::copy(&mut stream, &mut write_stream)
+                .expect("error in client thread: ");
+            println!("connection closed");
+        });
     }
-    Ok(())
-}
-
-fn grep_main() -> Result<(), Box<Error>> {
-    let mut args = std::env::args().skip(1);
-    let target = match args.next() {
-        Some(s) => s,
-        None => Err("usage: grep PATTERN FILE..")?
-    };
-    let files: Vec<PathBuf> = args.map(PathBuf::from).collect();
-
-    if files.is_empty() {
-        let stdin = io::stdin();
-        grep(&target, stdin.lock())?;
-    } else {
-        for file in files {
-            let f = File::open(file)?;
-            grep(&target, BufReader::new(f))?
-        }
-    }
-    Ok(())
 }
 
 fn main() {
-    let result = grep_main();
-    if let Err(err) = result {
-        let _ = writeln!(io::stderr(), "{}", err);
-    }
+    echo_main("127.0.0.1:17007").expect("error: ");
     println!("Hello, world!");
 }
