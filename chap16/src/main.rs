97eb0da3f233cdd7c18ad918c69d65b96d7f8742
ieiriyuki <ieiriyuki@sur.bbiq.jp>
2021-09-03 22:35:04 +0900

    Update 18.3 is done

--- chap16/src/main.rs
+++ chap16/src/main.rs
@@ -1,24 +1,27 @@
-use std::net::TcpListener;
-use std::io;
-use std::thread::spawn;
+extern crate reqwest;
 
-fn echo_main(addr: &str) -> io::Result<()> {
-    let listener = TcpListener::bind(addr)?;
-    println!("listening on {}", addr);
-    loop {
-        let (mut stream, addr) = listener.accept()?;
-        println!("connection received from {}", addr);
+use std::error::Error;
+use std::io::{self, Write};
 
-        let mut write_stream = stream.try_clone()?;
-        spawn(move || {
-            io::copy(&mut stream, &mut write_stream)
-                .expect("error in client thread: ");
-            println!("connection closed");
-        });
+fn http_get_main(url: &str) -> Result<(), Box<Error>> {
+    let mut response = reqwest::get(url)?;
+    if !response.status().is_success() {
+        Err(format!("{}", response.status()))?;
     }
+
+    let stdout = io::stdout();
+    io::copy(&mut response, &mut stdout.lock())?;
+
+    Ok(())
 }
 
 fn main() {
-    echo_main("127.0.0.1:17007").expect("error: ");
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 2 {
+        writeln!(io::stderr(), "usage: http-get URL").unwrap();
+        return;
+    }
+    if let Err(err) = http_get_main(&args[1]) {
+        writeln!(io::stderr(), "error: {}", err).unwrap();
+    }
 }
